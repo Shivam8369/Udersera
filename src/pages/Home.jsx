7fbf68e402786853abc057733cb02658f931d4e1
shivam sahu <sahu.sd@somaiya.edu>
2025-01-28 17:18:29 +0530

    added reviewSlider to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -177,7 +177,10 @@ function Home() {
       {/* Section 3 */}
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
         <InstructorSection />
-        <h1 className="text-center text-4xl font-semibold mt-8"> Reviews from other learners </h1>
+      </div>
+
+      <div className=' mb-16 mt-3'>
+        <h2 className='text-center text-2xl md:text-4xl font-semibold mt-8 text-richblack-5 mb-5'>Reviews from other learners</h2>
         <ReviewSlider />
       </div>
 
